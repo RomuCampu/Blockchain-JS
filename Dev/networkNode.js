98da943b1c0eee05481fdad70cce93698e3119a8
RomuCampu <romucampu@gmail.com>
2018-10-18 01:27:08 +0200

    Synchronized the network, added mine points, transactions

--- Dev/networkNode.js
+++ Dev/networkNode.js
@@ -5,147 +5,218 @@ const Blockchain = require('./blockchain');
 const uuid = require('uuid/v1');
 const port = process.argv[2];
 const rp = require('request-promise');
+
 const nodeAddress = uuid().split('-').join('');
+
 const beblock = new Blockchain();
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
 
-// GET THE BLOCKCHAIN //
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+// GET ENTIRE BLOCKCHAIN //
 app.get('/blockchain', function (req, res) {
   res.send(beblock);
 });
 
-// CREATE A NEW TRANSACTION // 
+
+// CREATE NEW TRANSACTION //
 app.post('/transaction', function (req, res) {
-      const newTransaction = req.body;
-      const blockIndex = beblock.addTransactionToPendingTransactions(newTransaction);
+  const newTransaction = req.body;
+  const blockIndex = beblock
+    .addTransactionToPendingTransactions(newTransaction);
+  res.json({
+    note: `Transaction will be added in block ${blockIndex}.`
+  });
+});
+
+
+// BROADCAST TRANSACTION //
+app.post('/transaction/broadcast', function (req, res) {
+  const newTransaction = beblock
+    .createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  beblock
+    .addTransactionToPendingTransactions(newTransaction);
+
+  const requestPromises = [];
+  beblock
+    .networkNodes.forEach(networkNodeUrl => {
+      const requestOptions = {
+        uri: networkNodeUrl + '/transaction',
+        method: 'POST',
+        body: newTransaction,
+        json: true
+      };
+
+      requestPromises.push(rp(requestOptions));
+    });
+
+  Promise.all(requestPromises)
+    .then(data => {
       res.json({
-        note: `Transaction will be added in block ${blockIndex}`
+        note: 'Transaction created and broadcast successfully.'
       });
+    });
+});
 
-      app.post('/transaction/broadcast', function (req, res) {
-        const newTransaction = beblock.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-        beblock.addTransactionToPendingTransactions(newTransaction);
 
-        const requestPromises = [];
-        beblock.networkNodes.forEach(networkNodeUrl => {
-          const requestOptions = {
-            uri: networkNodeUrl + '/transaction',
-            method: 'POST',
-            body: newTransaction,
-            json: true
-          };
-          requestOptions.push(rp(requestOptions));
-        });
+// MINE A BLOCK //
+app.get('/mine', function (req, res) {
+  const lastBlock = beblock
+    .getLastBlock();
+  const previousBlockHash = lastBlock['hash'];
+  const currentBlockData = {
+    transactions: beblock
+      .pendingTransactions,
+    index: lastBlock['index'] + 1
+  };
+  const nonce = beblock.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = beblock.hashBlock(previousBlockHash, currentBlockData, nonce);
+  const newBlock = beblock.createNewBlock(nonce, previousBlockHash, blockHash);
 
-        Promise.all(requestPromises).then(data => {
-          res.json({
-            note: 'Transaction created and broadcast with succes'
-          })
-        });
+  const requestPromises = [];
+  beblock.networkNodes.forEach(networkNodeUrl => {
+      const requestOptions = {
+        uri: networkNodeUrl + '/receive-new-block',
+        method: 'POST',
+        body: {
+          newBlock: newBlock
+        },
+        json: true
+      };
+
+      requestPromises.push(rp(requestOptions));
+    });
+
+  Promise.all(requestPromises)
+    .then(data => {
+      const requestOptions = {
+        uri: beblock
+          .currentNodeUrl + '/transaction/broadcast',
+        method: 'POST',
+        body: {
+          amount: 12.5,
+          sender: "00",
+          recipient: nodeAddress
+        },
+        json: true
+      };
+
+      return rp(requestOptions);
+    })
+    .then(data => {
+      res.json({
+        note: "New block mined & broadcast successfully",
+        block: newBlock
       });
+    });
+});
 
 
-      // MINE A BLOCK //
-      app.get('/mine', function (req, res) {
-        const lastBlock = beblock.getLastBlock();
-        const previousBlockHash = lastBlock['hash'];
-        const currentBlockData = {
-          transactions: beblock.pendingTransactions,
-          index: lastBlock['index'] + 1
-        };
+// RECEIVE NEW BLOCK //
+app.post('/receive-new-block', function (req, res) {
+  const newBlock = req.body.newBlock;
+  const lastBlock = beblock
+    .getLastBlock();
+  const correctHash = lastBlock.hash === newBlock.previousBlockHash;
+  const correctIndex = lastBlock['index'] + 1 === newBlock['index'];
 
-        const nonce = beblock.proofOfWork(previousBlockHash, currentBlockData);
-        const blockHash = beblock.hashBlock(
-          previousBlockHash,
-          currentBlockData,
-          nonce
-        );
+  if (correctHash && correctIndex) {
+    beblock
+      .chain.push(newBlock);
+    beblock
+      .pendingTransactions = [];
+    res.json({
+      note: 'New block received and accepted.',
+      newBlock: newBlock
+    });
+  } else {
+    res.json({
+      note: 'New block rejected.',
+      newBlock: newBlock
+    });
+  }
+});
 
-        beblock.createNewTransaction(25, '00');
+// REGISTER A NODE AND BROADCAST IT//
+app.post('/register-and-broadcast-node', function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl;
+  if (beblock
+    .networkNodes.indexOf(newNodeUrl) == -1) beblock
+    .networkNodes.push(newNodeUrl);
 
-        const newBlock = beblock.createNewBlock(
-          nonce,
-          previousBlockHash,
-          blockHash
-        );
+  const regNodesPromises = [];
+  beblock
+    .networkNodes.forEach(networkNodeUrl => {
+      const requestOptions = {
+        uri: networkNodeUrl + '/register-node',
+        method: 'POST',
+        body: {
+          newNodeUrl: newNodeUrl
+        },
+        json: true
+      };
 
-        res.json({
-          note: 'New block mined succesfully',
-          block: newBlock
-        });
+      regNodesPromises.push(rp(requestOptions));
+    });
+
+  Promise.all(regNodesPromises)
+    .then(data => {
+      const bulkRegisterOptions = {
+        uri: newNodeUrl + '/register-nodes-bulk',
+        method: 'POST',
+        body: {
+          allNetworkNodes: [...beblock
+            .networkNodes, beblock
+            .currentNodeUrl
+          ]
+        },
+        json: true
+      };
+
+      return rp(bulkRegisterOptions);
+    })
+    .then(data => {
+      res.json({
+        note: 'New node registered with network successfully.'
       });
+    });
+});
 
-      //Register a node and broadcast it to the network
-      app.post('/register-and-broadcast-node', function (req, res) {
-        const newNodeUrl = req.body.newNodeUrl;
-        beblock.networkNodes.push(newNodeUrl);
-        if (beblock.networkNodes.indexOf(newNodeUrl) === -1) beblock.networkNodes.push(newNodeUrl);
+// REGISTER A NODE WITH THE NETWORK //
+app.post('/register-node', function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl;
+  const nodeNotAlreadyPresent = beblock
+    .networkNodes.indexOf(newNodeUrl) == -1;
+  const notCurrentNode = beblock
+    .currentNodeUrl !== newNodeUrl;
+  if (nodeNotAlreadyPresent && notCurrentNode) beblock
+    .networkNodes.push(newNodeUrl);
+  res.json({
+    note: 'New node registered successfully.'
+  });
+});
 
-        const regNodesPromises = [];
-        beblock.networkNodes.forEach(networkNodeUrl => {
-          const requestOptions = {
-            uri: networkNodeUrl + '/register-node',
-            method: 'POST',
-            body: {
-              newNodeUrl: newNodeUrl
-            },
-            json: true
-          };
+// REGISTER MULTIPLE NODES AT ONCE //
+app.post('/register-nodes-bulk', function (req, res) {
+  const allNetworkNodes = req.body.allNetworkNodes;
+  allNetworkNodes.forEach(networkNodeUrl => {
+    const nodeNotAlreadyPresent = beblock
+      .networkNodes.indexOf(networkNodeUrl) == -1;
+    const notCurrentNode = beblock
+      .currentNodeUrl !== networkNodeUrl;
+    if (nodeNotAlreadyPresent && notCurrentNode) beblock
+      .networkNodes.push(networkNodeUrl);
+  });
 
-          regNodesPromises.push(rp(requestOptions));
-        });
+  res.json({
+    note: 'Bulk registration successful.'
+  });
+});
 
-        Promise.all(regNodesPromises).then(data => {
-          const bulkRegisterOptions = {
-            uri: newNodeUrl + '/register-nodes-bulk',
-            method: 'POST',
-            body: {
-              allNetworkNodes: [...beblock.networkNodes, beblock.currentNodeUrl]
-            },
-            json: true
-          };
-
-          return rp(bulkRegisterOptions);
-        }).then(data => {
-          res.json({
-            note: 'New node registered with network successfully'
-          });
-        });
-      });
-
-      // Register a node with the network
-      app.post('/register-node', function (req, res) {
-        const newNodeUrl = req.body.newNodeUrl;
-        const nodeNotAlReadyPresent = beblock.networkNodes.indexOf(newNodeUrl) === -1;
-        const notCurrentNode = beblock.currentNodeUrl !== newNodeUrl;
-        if (nodeNotAlReadyPresent && notCurrentNode) beblock.networkNodes.push(newNodeUrl);
-        res.json({
-          note: 'New node registered with the network'
-        });
-      });
-
-      // Register multiple nodes at once
-      app.post('/register-nodes-bulk', function (req, res) {
-        const allNetworkNodes = req.body.allNetworkNodes;
-        allNetworkNodes.forEach(networkNodeUrl => {
-          const nodeNotAlreadyPresent = beblock.networkNodes.indexOf(networkNodeUrl) == -1;
-          const notCurrentNode = beblock.currentNodeUrl !== networkNodeUrl;
-          if (nodeNotAlreadyPresent && notCurrentNode) beblock.networkNodes.push(networkNodeUrl);
-        });
-
-        res.json({
-          note: 'Bulk registration successful.'
-        });
-      });
-
-
-
-      app.listen(port, function () {
-        console.log(`Listening on port ${port}...`);
-      });
+app.listen(port, function () {
+  console.log(`Listening on port ${port}...`);
+});
